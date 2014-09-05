91e4777c8b1262c3a02c3c28eb1bc9c1baac2821
stevejpurves <stevejpurves@gmail.com>
2014-09-05 23:17:48 +0100

    now connecting to mongo

--- server/app.js
+++ server/app.js
@@ -1,16 +1,37 @@
 var express = require('express');
-var body_parser = require('body-parser');
+var bodyParser = require('body-parser');
 var api = require('./api');
+var mongoose = require('mongoose');
 
-module.exports = function(callback) {
+function startDB(callback) {
+	var db = mongoose.connection;
+
+	db.on('error', console.error);
+	db.once('open', function() {
+	  // Create your schemas and models here.
+	  	callback(db);
+	});
+
+	mongoose.connect('mongodb://localhost/dev');
+};
+
+function startServer(db, doListen) {
 	var app = express();
+	app.set('db', db);
 
 	// do serverside app config in here
-	app.use(body_parser());
+	app.use(bodyParser.urlencoded({ extended: false }))
+	app.use(bodyParser.json())
 	app.use(express.favicon());
 	app.use(express.methodOverride());
 	app.use(express.cookieParser("CANA12RIA1S*2KA(K£NAIO"));
 	app.use(express.session());
 
-	callback(app);
+	doListen(app);
+};
+
+module.exports = function(doListen) {
+	startDB( function(db) { 
+		startServer(db, doListen); 
+	} );
 };
